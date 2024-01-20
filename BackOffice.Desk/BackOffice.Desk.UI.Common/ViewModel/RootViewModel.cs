f09472b3a59b4cf268809e4977a67d870b2c18a0
akuklinov <akuklinov@bmfn.com>
2024-01-20 23:54:27 +0200

    Implementing mvvm basics.

---
+++ BackOffice.Desk/BackOffice.Desk.UI.Common/ViewModel/RootViewModel.cs
@@ -0,0 +1,19 @@
+﻿using BackOffice.Desk.UI.Common.ViewModel.Sections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackOffice.Desk.UI.Common.ViewModel
+{
+    public class RootViewModel: ViewModelBase
+    {
+        public ClientsViewModel Clients { get; private set; }
+
+        public RootViewModel()
+        {
+            this.Clients = new ClientsViewModel();
+        }
+    }
+}
--- BackOffice.Desk/BackOffice.Desk.UI/MainWindow.xaml.cs
+++ BackOffice.Desk/BackOffice.Desk.UI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BackOffice.Desk.UI.Common.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace BackOffice.Desk.UI
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new RootViewModel();
         }
     }
 }
